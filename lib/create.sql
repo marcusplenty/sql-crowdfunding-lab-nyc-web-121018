516a57a7109b6fa74a4db8f57a77623fe64f2b4c
Marcus Plenty <marcusplenty@gmail.com>
2018-12-20 15:33:16 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -8,8 +8,9 @@ CREATE TABLE projects (
     id INTEGER PRIMARY KEY,
     title TEXT,
     category TEXT,
+    funding_goal INTEGER,
+    start_date TEXT,
+    end_date TEXT,
     
-    name TEXT,
-    age INTEGER
 );
 
